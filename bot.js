b52b4d8b9ff6adcd34c675ee2511b74082d14f97
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2019-06-01 17:53:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,14 +22,14 @@ function master(msg) {
 		if (err) {
 			throw err;
 		}
-		if (result[0].Master) {
+		if (result[0] != 'undefined' and result[0].Master) {
 			msg.guild.members.map(member => 
 					      	{if(msg.content.indexOf(member.user.username) != -1) {
 							sql.query("UPDATE `Valchercher1` SET `Master` = 1 WHERE `Server_ID` = " + member.guild.id + " AND `Name_ID` = " + member.id, function (err) { if (err) throw err; });
 						}});
 		}
 		else {
-			sql.query("SELECT * FROM `Activity_bot` WHERE `Server_ID` = " + msg.member.guild.id + " AND `Master` = 1", function (err, result, fields) {
+			sql.query("SELECT * FROM `Valchercher1` WHERE `Server_ID` = " + msg.member.guild.id + " AND `Master` = 1", function (err, result, fields) {
 				if (err) {
 					throw err;
 				}
